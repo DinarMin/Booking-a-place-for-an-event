8ab2ca9fdbc2bd434747b1e60523af14317fcab4
Dinar2202 <dinar2202@gmail.com>
2025-11-13 12:02:03 +0300

    feat: table append function

--- src/db/postgres.ts
+++ src/db/postgres.ts
@@ -1,14 +1,33 @@
 import pg from "pg";
+import { readFile } from "fs/promises";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: "user",
-  host: "db",
+  user: "postgres",
+  host: "localhost",
   database: "mytestdb",
-  password: "admin123",
+  password: "dinar2202",
   port: 5432,
 });
+
+export async function initDB() {
+  const __dirname = path.dirname(__filename);
+  const sql = path.join(__dirname, "..", "..", "src", "db", "init.sql");
+
+  try {
+    const sqlDB = await readFile(sql, "utf-8");
+    await pool.query(sqlDB);
+    console.log("Таблицы events, booking успешно добавлены в базу данных"); 3 
+  } catch (error) {
+    console.error(
+      "Произошла ошибка, таблицы events, booking не добавлены в базу данных.",
+      error
+    );
+  }
+}
 
 pool.on("connect", () => {
   console.log("Успешный запрос к базе");
