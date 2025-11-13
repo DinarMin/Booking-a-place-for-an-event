29387080d5bdcf9ddbfe84f748b57114cc1f677d
Dinar2202 <dinar2202@gmail.com>
2025-11-13 09:02:42 +0300

    feat: Creating an HTTP server on Express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-const express = require('express');
+import express from "express";
+import bookingRoutes from "./routes/bookingRoutes";
+
+const app = express();
+
+app.listen(3000, () => console.log(" Сервер запущен http://localhost:3000"))
+
+app.use(express.json());
+
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/events", eventRoutes);
